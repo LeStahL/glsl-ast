472e52126ccab4fd884ae3cf95a1555d66435d60
Alexander Kraus <a.kraus@glp.de>
2023-03-07 12:49:34 +0100

    Added failing shader file test

---
+++ tests/glsl_ast/test_ShaderFile.py
@@ -0,0 +1,44 @@
+from sys import path
+from os.path import join, dirname
+path.append(join(dirname(__file__), '../../src'))
+
+from glsl_ast.VersionDirective import VersionDirective
+from glsl_ast.ASTVisitor import ASTVisitor
+from glsl_ast import grammar
+
+from unittest import TestCase, main
+from parsimonious import ParseError
+
+class TestShaderFile(TestCase):
+    EmptyShaderFile = """
+#version 450
+"""
+    ShaderFileWithMain = """
+#version 450 es
+
+void main() {
+
+}
+"""
+    NontrivialBodyShaderFile = """
+#version 450
+
+float x;
+vec2 y = 5;
+
+void main() {
+
+}
+"""
+
+    ASTVisitorInstance = ASTVisitor()
+
+    def test_EmptyShaderFile(self):
+        shaderFile = TestShaderFile.ASTVisitorInstance.visit(grammar.parse(TestShaderFile.EmptyShaderFile))
+        self.assertEqual(shaderFile.version, VersionDirective(450))
+        self.assertListEqual(shaderFile.body, [])
+
+    # TODO: Test two other cases.
+
+if __name__ == '__main__':
+    main()
